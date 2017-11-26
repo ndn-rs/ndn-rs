cff074cdab54e5b618eb9a521aeabfdc29a74317
Cyril Plisko <cyril.plisko@mountall.com>
2017-11-26 08:57:21 +0200

    Update crate
    1. Add clippy pedantic
    2. Move to gitlab

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+//! Named Data Networking (aka Internet 2.0)
+//!
+#![doc(html_root_url = "https://docs.rs/ndn/0.0.1")]
+#![cfg_attr(all(feature = "cargo-clippy", feature = "pedantic"), warn(clippy_pedantic))]
+
 extern crate bytes;
 
 pub mod error;
