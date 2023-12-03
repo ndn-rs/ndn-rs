0be98e4a79c5c412750a2832d24bbb73795bfba3
Cyril Plisko <cyril.plisko@mountall.com>
2023-12-03 21:48:30 +0200

    Refactor Type into newtype with constants

--- tlv/src/lib.rs
+++ tlv/src/lib.rs
@@ -50,133 +50,130 @@ mod string;
 
 mod impls;
 
-#[derive(Clone, Copy, Debug, PartialEq)]
-#[repr(u64)]
-pub enum Type {
-    Unassigned = 0, // Shouldn't be used
-    ImplicitSha256DigestComponent = 0x01,
-    ParametersSha256DigestComponent = 0x02,
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+pub struct Type(u64);
+
+#[allow(non_upper_case_globals)]
+impl Type {
+    pub const Unassigned: Self = Self(0);
+
+    pub const ImplicitSha256DigestComponent: Self = Self(1);
+    pub const ParametersSha256DigestComponent: Self = Self(2);
     // 3 Unassigned
     // 4 Unassigned
-    Interest = 0x05,
-    Data = 0x06,
-    Name = 0x07,
-    GenericNameComponent = 0x08,
+    pub const Interest: Self = Self(5);
+    pub const Data: Self = Self(6);
+    pub const Name: Self = Self(7);
+    pub const GenericNameComponent: Self = Self(8);
     // 9 (0x09) Reserved (formerly Selectors)
-    Nonce = 0x0a,
+    pub const Nonce: Self = Self(10);
     // 11 (0x0b) Reserved (formerly Scope)
-    InterestLifetime = 0x0c,
+    pub const InterestLifetime: Self = Self(12);
     // 13 (0x0d) Reserved (formerly MinSuffixComponents)
     // 14 (0x0e) Reserved (formerly MaxSuffixComponents)
     // 15 (0x0f) Reserved (formerly PublisherPublicKeyLocator)
     // 16 (0x10) Reserved (formerly Exclude)
     // 17 (0x11) Reserved (formerly ChildSelector)
-    MustBeFresh = 0x12,
+    pub const MustBeFresh: Self = Self(18);
     // 19 (0x13) Reserved (formerly Any)
-    MetaInfo = 0x14,
-    Content = 0x15,
-    SignatureInfo = 0x16,
-    SignatureValue = 0x17,
-    ContentType = 0x18,
-    FreshnessPeriod = 0x19,
-    FinalBlockId = 0x1a,
-    SignatureType = 0x1b,
-    KeyLocator = 0x1c,
-    KeyDigest = 0x1d,
-    ForwardingHint = 0x1e,
+    pub const MetaInfo: Self = Self(20);
+    pub const Content: Self = Self(21);
+    pub const SignatureInfo: Self = Self(22);
+    pub const SignatureValue: Self = Self(23);
+    pub const ContentType: Self = Self(24);
+    pub const FreshnessPeriod: Self = Self(25);
+    pub const FinalBlockId: Self = Self(26);
+    pub const SignatureType: Self = Self(27);
+    pub const KeyLocator: Self = Self(28);
+    pub const KeyDigest: Self = Self(29);
+    pub const ForwardingHint: Self = Self(30);
     // 31 (0x1f) Reserved (formerly Delegation)
-    KeywordNameComponent = 0x20,
-    CanBePrefix = 0x21,
-    HopLimit = 0x22,
+    pub const KeywordNameComponent: Self = Self(32);
+    pub const CanBePrefix: Self = Self(33);
+    pub const HopLimit: Self = Self(34);
     // 35 0x23 Reserved
-    ApplicationParameters = 0x24,
+    pub const ApplicationParameters: Self = Self(36);
     // 37 0x25 Reserved
-    SignatureNonce = 0x26,
-    SignatureTime = 0x28,
-    SignatureSeqNum = 0x2a,
-    InterestSignatureInfo = 0x2c,
-    InterestSignatureValue = 0x2e,
-    Fragment = 0x50,
-    Sequence = 0x51,
-    FragIndex = 0x52,
-    FragCount = 0x53,
-    HopCount = 0x54,
-    GeoTag = 0x55,
-    PitToken = 0x62,
-    LpPacket = 0x64,
-    ControlResponse = 0x65,
-    StatusCode = 0x66,
-    StatusText = 0x67,
-    ControlParameters = 0x68,
-    FaceId = 0x69,
-    Cost = 0x6a,
-    Strategy = 0x6b,
-    Flags = 0x6c,
-    ExpirationPeriod = 0x6d,
+    pub const SignatureNonce: Self = Self(38);
+    pub const SignatureTime: Self = Self(40);
+    pub const SignatureSeqNum: Self = Self(42);
+    pub const InterestSignatureInfo: Self = Self(44);
+    pub const InterestSignatureValue: Self = Self(46);
+    pub const SegmentNameComponent: Self = Self(50);
+    pub const ByteOffsetNameComponent: Self = Self(52);
+    pub const VersionNameComponent: Self = Self(54);
+    pub const TimestampNameComponent: Self = Self(56);
+    pub const SequenceNumNameComponent: Self = Self(58);
+    pub const Fragment: Self = Self(0x50);
+    pub const Sequence: Self = Self(0x51);
+    pub const FragIndex: Self = Self(0x52);
+    pub const FragCount: Self = Self(0x53);
+    pub const HopCount: Self = Self(0x54);
+    pub const GeoTag: Self = Self(0x55);
+    pub const PitToken: Self = Self(0x62);
+    pub const LpPacket: Self = Self(0x64);
+    pub const ControlResponse: Self = Self(101);
+    pub const StatusCode: Self = Self(102);
+    pub const StatusText: Self = Self(103);
+    pub const ControlParameters: Self = Self(104);
+    pub const FaceId: Self = Self(105);
+    pub const Cost: Self = Self(106);
+    pub const Strategy: Self = Self(107);
+    pub const Flags: Self = Self(108);
+    pub const ExpirationPeriod: Self = Self(109);
     // 110 (0x6e) reserved, (formerly LocalControlFeature)
-    Origin = 0x6f,
-    Mask = 0x70,
-    Uri = 0x72,
-    FaceStatus = 0x80,
-    LocalUri = 0x81,
-    ChannelStatus = 0x82,
-    Capacity = 0x83,
-    // UriScheme = 0x83, // conflicts with Capacity, defined as const
-    Count = 0x84,
-    // FaceScope = 0x84, // conflicts with Count, defined as const
-    FacePersistency = 0x85,
-    LinkType = 0x86,
-    BaseCongestionMarkingInterval = 0x87,
-    DefaultCongestionThreshold = 0x88,
-    Mtu = 0x89,
-    NInInterests = 0x90,
-    NInData = 0x91,
-    NOutInterests = 0x92,
-    NOutData = 0x93,
-    NInBytes = 0x94,
-    NOutBytes = 0x95,
-    FaceQueryFilter = 0x96,
-    NInNacks = 0x97,
-    NOutNacks = 0x98,
-    FaceEventNotification = 0xc0,
-    FaceEVentKind = 0xc1,
+    pub const Origin: Self = Self(111);
+    pub const Mask: Self = Self(112);
+    // 113 reserved
+    pub const Uri: Self = Self(114);
+    pub const FaceStatus: Self = Self(0x80);
+    pub const LocalUri: Self = Self(129);
+    pub const ChannelStatus: Self = Self(130);
+    pub const Capacity: Self = Self(131);
+    pub const UriScheme: Self = Self::Capacity; // conflicts with Capacity
+    pub const Count: Self = Self(132);
+    pub const FaceScope: Self = Self::Count; // conflicts with Count
+    pub const FacePersistency: Self = Self(0x85);
+    pub const LinkType: Self = Self(0x86);
+    pub const BaseCongestionMarkingInterval: Self = Self(135);
+    pub const DefaultCongestionThreshold: Self = Self(136);
+    pub const Mtu: Self = Self(137);
+    pub const NInInterests: Self = Self(0x90);
+    pub const NInData: Self = Self(0x91);
+    pub const NOutInterests: Self = Self(0x92);
+    pub const NOutData: Self = Self(0x93);
+    pub const NInBytes: Self = Self(0x94);
+    pub const NOutBytes: Self = Self(0x95);
+    pub const FaceQueryFilter: Self = Self(0x96);
+    pub const NInNacks: Self = Self(0x97);
+    pub const NOutNacks: Self = Self(0x98);
+    pub const FaceEventNotification: Self = Self(0xc0);
+    pub const FaceEVentKind: Self = Self(0xc1);
     // 192 (0xc2) (reserved, formerly FaceFlags)
-    Nack = 0x0320,
-    NackReason = 0x0321,
-    IncomingFaceId = 0x032c,
-    NextHopFaceId = 0x0330,
+    pub const Nack: Self = Self(0x0320);
+    pub const NackReason: Self = Self(0x0321);
+    pub const IncomingFaceId: Self = Self(0x032c);
+    pub const NextHopFaceId: Self = Self(0x0330);
     // 821 0x0331 Reserved, formerly IncomingFaceId
-    CachePolicy = 0x0334,
-    CachePolicyType = 0x0335,
-    CongestionMark = 0x0340,
-    Ack = 0x0344,
-    TxSequence = 0x0348,
-    NonDiscovery = 0x034c,
-    PrefixAnnouncement = 0x0350,
-    ValidityPeriod = 0xfd,
-    NotBefore = 0xfe,
-    NotAfter = 0xff,
-    AdditionalDescription = 0x0102,
-    DescriptionEntry = 0x0200,
-    DescriptionKey = 0x0201,
-    DescriptionValue = 0x0202,
-    Unknown(u64),
-}
-
-#[allow(non_upper_case_globals)]
-impl Type {
-    pub const UriScheme: Self = Self::Capacity;
-    pub const FaceScope: Self = Self::Count;
+    pub const CachePolicy: Self = Self(0x0334);
+    pub const CachePolicyType: Self = Self(0x0335);
+    pub const CongestionMark: Self = Self(0x0340);
+    pub const Ack: Self = Self(0x0344);
+    pub const TxSequence: Self = Self(0x0348);
+    pub const NonDiscovery: Self = Self(0x034c);
+    pub const PrefixAnnouncement: Self = Self(0x0350);
+    pub const ValidityPeriod: Self = Self(0xfd);
+    pub const NotBefore: Self = Self(0xfe);
+    pub const NotAfter: Self = Self(0xff);
+    pub const AdditionalDescription: Self = Self(0x0102);
+    pub const DescriptionEntry: Self = Self(0x0200);
+    pub const DescriptionKey: Self = Self(0x0201);
+    pub const DescriptionValue: Self = Self(0x0202);
 }
 
 impl From<VarNumber> for Type {
     fn from(n: VarNumber) -> Self {
-        let n = n.to_u64();
-        Self::from(n)
-        // if n > u64::from(u16::MAX) {
-        //     return Err(io::Error::other("Invalid Type `{n}`"));
-        // }
-        // Ok(n as Self)
+        Self(n.to_u64())
     }
 }
 
