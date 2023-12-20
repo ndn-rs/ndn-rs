de12493796ecb0f0727d5960d7e10caf9a8f5109
Cyril Plisko <cyril.plisko@mountall.com>
2023-12-21 01:08:01 +0200

    Back to use GenericArray for digest

--- tlv/src/name/component/digest/implicit.rs
+++ tlv/src/name/component/digest/implicit.rs
@@ -3,8 +3,7 @@ use super::*;
 #[derive(Clone, Debug, PartialEq, Eq, Hash, Tlv)]
 #[tlv(r#type = Type::ImplicitSha256DigestComponent, error = DecodeError)]
 pub struct ImplicitSha256DigestComponent {
-    digest: [u8; 32],
-    // digest: GenericArray<u8, U32>,
+    digest: GenericArray<u8, U32>,
 }
 
 impl ImplicitSha256DigestComponent {
@@ -12,7 +11,6 @@ impl ImplicitSha256DigestComponent {
     pub const PREFIX_NUMERIC: &'static str = "1";
 
     pub fn new(digest: impl Into<GenericArray<u8, U32>>) -> Self {
-        let digest = digest.into();
         let digest = digest.into();
         Self { digest }
     }
@@ -25,8 +23,7 @@ impl TryFrom<Generic> for ImplicitSha256DigestComponent {
         let digest = generic
             .check_type(Type::ImplicitSha256DigestComponent)?
             .check_length(GenericArray::<u8, U32>::len())?
-            .try_into_generic_array()?
-            .into();
+            .try_into_generic_array()?;
 
         Ok(Self { digest })
     }
@@ -42,7 +39,6 @@ impl str::FromStr for ImplicitSha256DigestComponent {
 
 impl fmt::Display for ImplicitSha256DigestComponent {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let array = GenericArray::<u8, U32>::from_slice(&self.digest);
-        format!("{}={:x}", Self::PREFIX, array).fmt(f)
+        format!("{}={:x}", Self::PREFIX, self.digest).fmt(f)
     }
 }
